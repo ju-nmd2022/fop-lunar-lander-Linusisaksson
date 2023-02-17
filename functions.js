a1479d5661d2c21194a9b4728c090dd796bb0b09
Linus Isaksson <linusisaksson97@gmail.com>
2023-02-17 16:52:42 +0100

    Added prelimanary game states

---
+++ functions.js
@@ -0,0 +1,8 @@
+//trying to see if i can call a function from another
+// js file into the main one so the code is less crowded
+
+function ball() {
+  fill(0, 0, 255);
+  ellipse(200, 200, 100);
+}
+ball();
---
+++ game.js
@@ -0,0 +1,51 @@
+//State of games
+
+let state = "start";
+
+function StartScreen() {
+  background(255, 200, 100);
+  text("To start game press any key", 100, 100);
+  //   console.log("startscreen is active");
+}
+
+//all code that is run when game is active
+function GameScreen() {
+  background(200, 200, 100);
+  text("game is running", 200, 200);
+  console.log("game is running");
+}
+//code that is run when you win
+function WinScreen() {
+  background(255, 255, 0);
+  text("you win", 250, 250);
+  console.log("winner");
+}
+//code that is run when you lose
+function LoseScreen() {
+  background(255, 50, 0);
+  text("you lose 💀", 300, 300);
+  console.log("loser");
+}
+// StartScreen();
+// GameScreen();
+// WinScreen();
+
+function draw() {
+  if (state === "start") {
+    StartScreen();
+    // if (state === "start" && keyIsDown(32)) {
+    //   state === "game";
+    //   console.log("game");
+    // }
+  } else if (state === "game") {
+    GameScreen();
+  } else if (state === "win") {
+    WinScreen();
+  } else if (state === "lose") {
+    LoseScreen();
+  }
+}
+
+function keyPressed() {
+  state = "game";
+}
